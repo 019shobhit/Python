1959506cbff0d7609290d082daf72a417203c382
019shobhit <019shobhit@gmail.com>
2024-09-12 12:29:39 +0530

    push all codes

---
+++ Code With Harry/Chapter 1/first.py
@@ -0,0 +1,8 @@
+print("Hello \nShobhit") 
+# This is Comment
+x = 4
+y = x
+print(y)
+y= "shobhit"
+print(x)
+print(y)
---
+++ College Wallah/variables.py
@@ -0,0 +1,9 @@
+name = "Shobhit"
+age = 22.23
+gender = "male"
+
+print("name: " , name)
+print("Age: " , age+1)
+print("Gender: " , gender)
+
+print(name, age, gender , sep=" -> ")
