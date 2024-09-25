9c394dc4fdc3aa5b246710f6fe6f3a7883d730af
019shobhit <019shobhit@gmail.com>
2024-09-25 08:33:26 +0530

    new codes in 25-09-2024

---
+++ College Wallah/operators.py
@@ -0,0 +1,31 @@
+# Assingment Operators
+# n1 = float(input("Enter number: "))
+# n2 = n1
+# print(n2)
+# print(type(n1))
+# print(type(n2))
+
+
+# conditional operator
+# n1 = 2 > 5
+# n2 = 2 < 5
+# print("And : ",n1 and n2)
+# print("OR: ", n1 or n2)
+# print("NOT: ", not(n1))
+
+# identity operator
+# x = 5
+# y =5
+# print("if X is Y: ",x is y)
+# print("if x is not y: ",x is not y)
+
+# Membership operators
+# fruits = ["apple","graps","mango"]
+# print("is graps is present in the the fruits list: ", "grapps" in fruits)
+
+# Bitwise Operators
+a =5
+b = 3
+print("a AND b: ", a & b)
+print("a OR b: ", a | b)
+print("a XOR b: ", a ^ b)
---
+++ College Wallah/volumeOfSphere.py
@@ -0,0 +1,4 @@
+r = float(input("Enter radius of sphere to find the volume:"))
+pi = 3.1415
+volume = (4*pi*(r**3))/3
+print("Volume is : ",volume)
