25942c2030fe9a8d44eea83f789b3011ba50e836
019shobhit <019shobhit@gmail.com>
2024-11-25 11:08:53 +0530

    add new codes

--- College Code/audio.py
+++ College Code/audio.py
@@ -1,35 +1,19 @@
-# import pyttsx3 
+import pyttsx3 
 
-# engine = pyttsx3.init()
+engine = pyttsx3.init()
 
-# rate = engine.getProperty('rate')
-# print("Current rate:", rate)
+rate = engine.getProperty('rate')
+print("Current rate:", rate)
 
-# engine.setProperty('rate', 100)
+engine.setProperty('rate', 100)
 
-# voices = engine.getProperty('voices')
-# print("Available voices:", voices)
+voices = engine.getProperty('voices')
+print("Available voices:", voices)
 
-# engine.setProperty('voice', voices[0].id)
+engine.setProperty('voice', voices[0].id)
 
-# def SpeakText(command):
-#     engine.say(command)
-#     engine.runAndWait()
+def SpeakText(command):
+    engine.say(command)
+    engine.runAndWait()
 
-# SpeakText("manu bhai")
-
-
-
-import speech_recognition as sr 
-
-r = sr.Recognizer()
-with sr.Microphone() as source:
-    print("Speak anything...")
-    audio = r.listen(source)
-    try:
-        text = r.recognize_google(audio)
-        print("You said: {}".format(text))
-    except sr.UnknownValueError:
-        print("Couldn't catch you")
-    except sr.RequestError as e:
-        print("Could not request results from Google Speech Recognition service; {0}".format(e))
+SpeakText("manu bhai")
