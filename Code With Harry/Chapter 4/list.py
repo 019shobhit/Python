840a24a26ab4a14f348c10138bf22286304e91d7
019shobhit@gmail.com <019shobhit@gmail.com>
2025-07-01 13:50:52 +0530

    add all codes

---
+++ Code With Harry/Chapter 4/list.py
@@ -0,0 +1,7 @@
+arr =[4,4.5,12,3,4,56,7]
+
+# arr[1]="vanshi"
+
+arr.remove(12)
+
+print(arr)
---
+++ Code With Harry/Chaptre 4 PS/q1.py
@@ -0,0 +1,12 @@
+# 1. Write a program to store seven fruits in a list entered by the user
+
+a = input("fruit 1: ")
+b = input("fruit 1: ")
+c = input("fruit 1: ")
+d = input("fruit 1: ")
+e = input("fruit 1: ")
+f = input("fruit 1: ")
+g = input("fruit 1: ")
+
+l1 = {a,b,c,d,e,f,g}
+print(l1)
