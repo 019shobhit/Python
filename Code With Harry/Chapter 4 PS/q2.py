5d5640e3f3d2d1274d2dbbb8f8e89584e7602853
019shobhit@gmail.com <019shobhit@gmail.com>
2025-07-01 19:39:44 +0530

    add all codes

---
+++ Code With Harry/Chapter 4 PS/q2.py
@@ -0,0 +1,17 @@
+# 2. Write a program to accept marks of 6 students and display them in a sorted manner
+
+marks =[]
+
+m= int(input("Enter Marks: "))
+marks.append(m)
+m= int(input("Enter Marks: "))
+marks.append(m)
+m= int(input("Enter Marks: "))
+marks.append(m)
+m= int(input("Enter Marks: "))
+marks.append(m)
+m= int(input("Enter Marks: "))
+marks.append(m)
+m= int(input("Enter Marks: "))
+marks.sort()
+print(marks)
---
+++ Code With Harry/Chapter 4 PS/q4.py
@@ -0,0 +1,5 @@
+# 4. Write a program to sum a list with 4 numbers
+
+l1 = [1,2,4]
+print(sum(l1))
+    
